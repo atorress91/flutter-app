248ad07566142ba1a6fb24c0f3278181d269bc9a
AndresTS <andres91411@gmail.com>
2025-09-08 13:19:08 -0600

    Replace `StatCard` with unified `InfoCard` in `StatsCarousel` and remove `StatCard` widget
    - Updated `StatsCarousel` to use `InfoCard` for consistency and reusability.
    - Deleted `StatCard` widget as it is no longer required.

--- lib/features/dashboard/presentation/widgets/stats_carousel/stats_carousel.dart
+++ lib/features/dashboard/presentation/widgets/stats_carousel/stats_carousel.dart
@@ -1,14 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:my_app/core/common/widgets/info_card.dart';
 import 'package:my_app/core/l10n/app_localizations.dart';
 import 'package:my_app/features/dashboard/data/home_data.dart';
 import 'package:my_app/features/dashboard/domain/entities/balance_information.dart';
 
-import 'stat_card.dart';
-
 class StatsCarousel extends StatelessWidget {
   final BalanceInformation balance;
 
-  const StatsCarousel({super.key,required this.balance});
+  const StatsCarousel({super.key, required this.balance});
 
   @override
   Widget build(BuildContext context) {
@@ -23,11 +22,14 @@ class StatsCarousel extends StatelessWidget {
         separatorBuilder: (context, index) => const SizedBox(width: 16),
         itemBuilder: (context, index) {
           final stat = stats[index];
-          return StatCard(
-            title: AppLocalizations.of(context).t(stat['titleKey'] as String),
-            value: stat['value'] as String,
-            icon: stat['icon'] as IconData,
-            color: stat['color'] as Color,
+          return SizedBox(
+            width: 160,
+            child: InfoCard(
+              title: AppLocalizations.of(context).t(stat['titleKey'] as String),
+              value: stat['value'] as String,
+              icon: stat['icon'] as IconData,
+              color: stat['color'] as Color,
+            ),
           );
         },
       ),
